6b382027a401d9602ae6a4e17d8e12930d9a5544
Yrmee <y.eggermond@gmail.com>
2022-04-21 13:58:12 +0200

    Update Student.jsx

--- src/pages/student/Student.jsx
+++ src/pages/student/Student.jsx
@@ -10,6 +10,8 @@ import { studentDetails } from '../../studentDetailsData';
 import studentsData from '../../studentData.json';
 import Chart from '../../components/chart/Chart';
 
+// Note: Error in console for line: 37.1 (cannot reading 'firstName' - Trying to set the error page in router/path )
+
 
 // get studentDetails by ID
 const getStudentById = (firstName) => {
